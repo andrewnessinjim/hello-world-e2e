d33665e2399e3515a6ac834937308f5e6f51cb64
Andrew Nessin <andrewnessinjim@yahoo.com>
2021-12-09 23:01:41 +0530

    Listen for SIGINT and SIGTERM signals and exit

--- src/server/hello.js
+++ src/server/hello.js
@@ -7,3 +7,20 @@ http.createServer(function(req, res) {
 }).listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+process.on('SIGINT', function onSigint () {
+	console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+});
+
+// quit properly on docker stop
+process.on('SIGTERM', function onSigterm () {
+  console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+})
+
+// shut down server
+function shutdown() {
+    process.exitCode = 1;
+	process.exit();
+}
