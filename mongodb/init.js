3efa67c99283b2a481858a7e6dd9dd690a899e21
Andrew Nessin <andrewnessinjim@yahoo.com>
2021-12-15 20:03:14 +0530

    Automate MongoDB initialization for local

---
+++ mongodb/init.js
@@ -0,0 +1,21 @@
+let DB_NAMESPACE = "helloworld";
+db.createUser(
+    {
+        user: "hello-world-user",
+        pwd: "hello-world-pass",
+        roles: [
+            {
+                role: "readWrite",
+                db: DB_NAMESPACE
+            }
+        ]
+    }
+)
+
+db.auth("hello-world-user", "hello-world-pass");
+let appDb = db.getSiblingDB(DB_NAMESPACE);
+
+appDb.pages.insertOne({
+    pageName: "homePage",
+    message: "Hello from all of us"
+});
