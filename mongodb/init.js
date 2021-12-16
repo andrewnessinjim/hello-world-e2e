88c32384459255a5e7afa28a73df1339e65904ae
Andrew Nessin <andrewnessinjim@yahoo.com>
2021-12-16 18:08:13 +0530

    Display headings for each set of tools

--- mongodb/init.js
+++ mongodb/init.js
@@ -17,5 +17,129 @@ let appDb = db.getSiblingDB(DB_NAMESPACE);
 
 appDb.pages.insertOne({
     pageName: "homePage",
-    heading: "Hello World!"
+    heading: "Hello World!",
+    sections: [{
+            heading: "Source code in",
+            logos: [{
+                    logoKey : "typescript",
+                    toolTip: ""
+                },{
+                    logoKey : "pug",
+                    toolTip: ""
+                },{
+                    logoKey : "sass",
+                    toolTip: ""
+                },{
+                    logoKey : "svg",
+                    toolTip: ""
+                }]
+        },{
+            heading: "Using",
+            logos: [{
+                    logoKey : "apollo",
+                    toolTip: ""
+                },{
+                    logoKey : "express",
+                    toolTip: ""
+                },{
+                    logoKey : "graphql",
+                    toolTip: ""
+                },{
+                    logoKey : "reactjs",
+                    toolTip: ""
+                },{
+                    logoKey : "mongodb",
+                    toolTip: ""
+                }]
+        },{
+            heading: "Transformed by",
+            logos: [{
+                    logoKey : "gulp",
+                    toolTip: ""
+                },{
+                    logoKey : "webpack",
+                    toolTip: ""
+                },{
+                    logoKey : "babel",
+                    toolTip: ""
+                }]
+        },{
+            heading: "Transformed into",
+            logos: [{
+                    logoKey : "html",
+                    toolTip: ""
+                },{
+                    logoKey : "javascript",
+                    toolTip: ""
+                },{
+                    logoKey : "css",
+                    toolTip: ""
+                }]
+        },{
+            heading: "Executed in",
+            logos: [{
+                    logoKey : "nodejs",
+                    toolTip: ""
+                },{
+                    logoKey : "nodemon",
+                    toolTip: ""
+                },{
+                    logoKey : "browsers",
+                    toolTip: ""
+                }]
+        },{
+            heading: "Packaged into",
+            logos: [{
+                    logoKey : "docker",
+                    toolTip: ""
+                }]
+        },{
+            heading: "Test cases in",
+            logos: [{
+                    logoKey : "jest",
+                    toolTip: ""
+                },{
+                    logoKey : "cypress",
+                    toolTip: ""
+                }]
+        },{
+            heading: "Automatically built, tested and deployed by",
+            logos: [{
+                    logoKey : "docker",
+                    toolTip: ""
+                }
+            ]
+        },{
+            heading: "Deployed to",
+            logos: [{
+                    logoKey : "heroku",
+                    toolTip: ""
+                }
+            ]
+        },{
+            heading: "Dependencies managed by",
+            logos: [{
+                    logoKey : "npm",
+                    toolTip: ""
+                }
+            ]
+        },{
+            heading: "Version controlled by",
+            logos: [{
+                    logoKey : "git",
+                    toolTip: ""
+                },{
+                    logoKey : "github",
+                    toolTip: ""
+                }
+            ]
+        },{
+            heading: "IDE used",
+            logos: [{
+                    logoKey : "vscode",
+                    toolTip: ""
+                }
+            ]
+        }
+    ]
 });
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -16,6 +16,13 @@ const HOMEPAGE_QUERY = gql`
 query {
   homePage {
     heading
+    sections {
+      heading
+      logos {
+        logoKey
+        toolTip
+      }
+    }
   }
 }
 `
@@ -24,25 +31,35 @@ const client = new ApolloClient({
     cache: new InMemoryCache()
 })
 
-const SiteHeading = () => {
+const HomePage = () => {
     const {data} = useQuery(HOMEPAGE_QUERY);
-
     return (
-        <h1>
-            {data && (
-                <>
-                    {data.homePage.heading}
-                </>
-            )}
-        </h1>
-    )
+        <main>{data && (
+            <>
+                <Heading heading={data.homePage.heading}/>
+                {data.homePage.sections
+                .map((section, index) => 
+                    <Section key={index} heading={section.heading}/>
+                )}
+            </>
+        )}
+        </main>)
+}
+const Heading = (props) => {
+    return <h1>{props.heading}</h1>
+}
+
+const Section = (props) => {
+    return (
+        <section>
+            <h2>{props.heading}</h2>
+        </section>
+    );
 }
 
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <section>
-            <SiteHeading/>
-        </section>
+        <HomePage />
     </ApolloProvider>,
     document.getElementById("mount")
 );
